d0f684447627b8ba0affaa2c941764136b92beb9
vocterr <kozanecki.bartosz@interia.pl>
2025-01-05 17:41:30 +0100

    xd

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -19,8 +19,8 @@ router.post("/login", async (req: Request, res: Response): Promise<any> => {
         if (!isMatch) return res.status(400).json({error: "Invalid credentials"});
 
         const token = jwt.sign({userId: user.id}, "1234");
-        res.cookie("token", token, {httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 * 10});
-        res.cookie("loggedIn", "yes", { maxAge: 1000 * 60 * 60 * 24 * 365 * 10 });
+        res.cookie("token", token, {httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 365 * 10, sameSite: "lax"});
+        res.cookie("loggedIn", "yes", { maxAge: 1000 * 60 * 60 * 24 * 365 * 10, sameSite: "lax" });
         res.json(token);
     }
     catch(error) {
